e50ae12223afe81f99f842c53a1d3e849c5fc5a5
benjamin-andoh <benoo.andoh@gamil.com>
2023-06-16 12:31:04 +0000

    add secret key

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "6w9jv@^b@y7742ze_)+b%5-9s($w&=hn@n9jd41jpci=hbe6uj")
+# print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
